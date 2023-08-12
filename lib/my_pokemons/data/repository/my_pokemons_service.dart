1dbdb39820a2f3168a5bbbb58eac613634b729bf
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-08-12 15:12:28 +0100

    delete query setup

--- lib/my_pokemons/data/repository/my_pokemons_service.dart
+++ lib/my_pokemons/data/repository/my_pokemons_service.dart
@@ -3,26 +3,26 @@ import 'package:isar/isar.dart';
 import '../../../favorites/data/repository/isar_service.dart';
 import 'package:pokemon/my_pokemons/data/data/model/my_pokemons_model.dart';
 
-
 class MyPokemonService {
   Future<Isar> openIsar() async {
     return IsarService.isarInstance!;
   }
 
-  Future<void> createMyPokemon(
-      MyPokemonModel newMyPokemonModel
-      ) async {
+  Future<void> createMyPokemon(MyPokemonModel newMyPokemonModel) async {
     final isar = await openIsar();
     isar.writeTxnSync<int>(
-            () => isar.myPokemonModels.putSync(newMyPokemonModel));
+        () => isar.myPokemonModels.putSync(newMyPokemonModel));
   }
 
+  Future<void> deleteMyPokemon(int id) async {
+    final isar = await openIsar();
+    isar.writeTxnSync(() async => await isar.myPokemonModels.delete(id));
+  }
 
   Stream<List<MyPokemonModel>> getAllMyPokemon() async* {
     final isar = await openIsar();
     yield* isar.myPokemonModels.where().watch(fireImmediately: true);
   }
-
 
   savePokemon({
     required String name,
@@ -33,23 +33,20 @@ class MyPokemonService {
   }) async {
     final isar = await openIsar();
     final myPokemonDB = isar.myPokemonModels;
-      final existingItem =
-      myPokemonDB.where().filter().nameContains(name);
-      if (existingItem.isEmptySync()) {
-        // create a pokemon
-        final myPokemon = MyPokemonModel()
-          ..spriteUrl = spriteUrl
-          ..name = name
-          ..weight = weight
-          ..height = height
-          ..types = types;
-        createMyPokemon(myPokemon);
-      } else {
-
-        throw Exception("Pokemon Name Already Exist");
-      }
+    final existingItem = myPokemonDB.where().filter().nameContains(name);
+    if (existingItem.isEmptySync()) {
+      // create a pokemon
+      final myPokemon = MyPokemonModel()
+        ..spriteUrl = spriteUrl
+        ..name = name
+        ..weight = weight
+        ..height = height
+        ..types = types;
+      createMyPokemon(myPokemon);
+    } else {
+      throw Exception("Pokemon Name Already Exist");
+    }
   }
 }
 
-final myPokemonServiceProvider =
-Provider((ref) => MyPokemonService());
+final myPokemonServiceProvider = Provider((ref) => MyPokemonService());
