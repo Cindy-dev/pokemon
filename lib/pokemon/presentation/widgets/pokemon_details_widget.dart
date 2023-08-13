b43adb1d43c3f2dd67d91ef639f9f43d2c03933d
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-08-13 08:48:29 +0100

    checking for null value before converting to a different measurement type

--- lib/pokemon/presentation/widgets/pokemon_details_widget.dart
+++ lib/pokemon/presentation/widgets/pokemon_details_widget.dart
@@ -16,37 +16,39 @@ class PokemonDetailsWidget extends StatelessWidget {
           pokemonDetailModel.name ?? "",
           style: AppTextStyles.displayLarge.copyWith(fontSize: 30),
         ),
-        if(pokemonDetailModel.types != null)
-        Row(
-          //using the map function to extract the name property from each Type object
-          //then convert the mapped iterable into a list of widgets using toList
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:
-          pokemonDetailModel.types!.map((type) {
-            return Container(
-              margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-              decoration: BoxDecoration(
-                  color: context.colors.yellow,
-                  borderRadius: BorderRadius.circular(50)),
-              child: Text(
-                type.type?.name ?? "",
-                style: AppTextStyles.heading2Bold.copyWith(
-                    fontSize: 20, color: context.themeData.colorScheme.error),
-              ),
-            );
-          }).toList(),
-        ),
+        if (pokemonDetailModel.types != null)
+          Row(
+            //using the map function to extract the name property from each Type object
+            //then convert the mapped iterable into a list of widgets using toList
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: pokemonDetailModel.types!.map((type) {
+              return Container(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                decoration: BoxDecoration(
+                    color: context.colors.yellow,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Text(
+                  type.type?.name ?? "",
+                  style: AppTextStyles.heading2Bold.copyWith(
+                      fontSize: 20, color: context.themeData.colorScheme.error),
+                ),
+              );
+            }).toList(),
+          ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Column(
               children: [
-                Text(
-                  //dividing by 10 to convert from hectogram to kilogram
-                  "${pokemonDetailModel.weight ?? 0 / 10}kg",
-                  style: AppTextStyles.displayLarge.copyWith(fontSize: 30),
-                ),
+                if (pokemonDetailModel.weight != null)
+                  Text(
+                    //dividing by 10 to convert from hectogram to kilogram
+                    "${pokemonDetailModel.weight! / 10} kg",
+                    style: AppTextStyles.displayLarge.copyWith(fontSize: 30),
+                  ),
                 const Text(
                   "Weight",
                   style: AppTextStyles.bodyMedium,
@@ -56,11 +58,12 @@ class PokemonDetailsWidget extends StatelessWidget {
             const SizedBox(width: 30),
             Column(
               children: [
-                Text(
-                  //dividing by 10 to convert from decimeter to meters
-                  "${pokemonDetailModel.height ?? 0  / 10}m",
-                  style: AppTextStyles.displayLarge.copyWith(fontSize: 30),
-                ),
+                if (pokemonDetailModel.height != null)
+                  Text(
+                    //dividing by 10 to convert from decimeter to meters
+                    "${pokemonDetailModel.height! / 10}m",
+                    style: AppTextStyles.displayLarge.copyWith(fontSize: 30),
+                  ),
                 const Text(
                   "Height",
                   style: AppTextStyles.bodyMedium,
